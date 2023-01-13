b12c1929a9962d4cc3002a1b6c55c85730a1dc91
Evaoi <47578331+Evaoi@users.noreply.github.com>
2023-01-13 17:29:01 +0100

    Add first request

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # daily_weather_forecast
+
 Application permettant de récupérer les prévisions métérologiques de 10 villes belges
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::error::Error;
+
+use serde::{Deserialize};
+//use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct Weather {
+    location: Location,
+}
+
+#[derive(Debug, Deserialize)]
+struct Location {
+    name: String,
+    country: String,
+    //lat: f32,
+    //lon: f32,
+    //tz_id: String,
+    //localtime_epoch: i64,
+    localtime: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Weathers {
+    weathers: Vec<Weather>
+}
+
+fn get_weather(url: &str) -> Result<Weather, Box<dyn Error>> {
+    let response = ureq::get(url).call()?.into_string()?;
+
+    let weather: Weather = serde_json::from_str(&response)?;
+
+    Ok(weather)
+}
 fn main() {
-    println!("Hello, world!");
+    let url: &str = "http://api.weatherapi.com/v1/forecast.json?key=bd238e9b8e7944358d6190707231201&q=Braine-Alleud&lang=fr&days=1";
+    let weather = get_weather(url);
+
+    dbg!(weather);
 }
